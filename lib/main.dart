100c2cf86f6544a160922605ed773b472703982f
Manuel Pineda Mendez <manuel.pineda@sofka.com.co>
2022-07-21 08:31:31 -0500

    Edit profile picture interface

--- lib/main.dart
+++ lib/main.dart
@@ -9,21 +9,107 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+      home: EditProfileUI(),
+    );
+  }
+}
+
+class EditProfileUI extends StatefulWidget {
+  const EditProfileUI({Key? key}) : super(key: key);
+
+  @override
+  State<EditProfileUI> createState() => _EditProfileUIState();
+}
+
+class _EditProfileUIState extends State<EditProfileUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome to Flutter'),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ), onPressed: () {},
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        actions: [
+          IconButton(
+              icon: const Icon(
+                Icons.settings,
+                color: Colors.white,
+              ),
+              onPressed: () {  },
+          )
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.only(left: 15, top: 20, right: 15),
+        child: GestureDetector(
+          onTap: (){
+            FocusScope.of(context).unfocus();
+          },
+          child: ListView(
+            children: [
+              Center(
+                child: Stack(
+                  children: [
+                    Container(
+                      width: 130,
+                      height: 130,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 4,
+                          color: Colors.white
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            spreadRadius: 2,
+                            blurRadius: 10,
+                            color: Colors.black.withOpacity(0.1)
+                          )
+                        ],
+                        shape: BoxShape.circle,
+                        image: const DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                            'https://wesofka.gointegro.com/scontent/1390/profile/6dfe9fc881c6d6e69873fe4703d46d9151d89bed?width=100&height=100'
+                          )
+                        )
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            width: 4,
+                            color: Colors.white
+                          ),
+                          color: Colors.blue
+                        ),
+                        child: const Icon(
+                          Icons.edit,
+                          color: Colors.white,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-
 
 
 
